ffab0ec7bcae01654f6ce6d12e4131ad5247e8b5
gustavopazbarreto47-beep <gustavopazbarreto47@gmai.com>
2026-07-20 21:17:39 -0300

    crud melhorado

--- BD/teste_bd.sql
+++
@@ -1,6 +0,0 @@
-SELECT * from Agenda;
-INSERT INTO Agenda (data,horario,descricao) VALUES ("16/07","!3:30","Treino");
-UPDATE Agenda
-SET descricao = "eu"
-WHERE id = 3;
-DELETE FROM agenda WHERE id = 2;
--- start.py
+++ start.py
@@ -1,115 +1,136 @@
-
-agendas = []
+# listas
 notas = []
+agendas = []
 tarefas = []
-
+# entrar no menu 
 while True:
     print("""
-    =========================
-          ASSISTENTE GP
-    =========================
-
-    1 - Agenda
-    2 - Bloco de notas
-    3 - Lista de tarefas
-    4 - Configurações
-    5 - Sair
+    ====================
+       Assistente GP   
+    ====================
         
-    """)
-    opçao = int(input("Escolha: "))
+    1 - Bloco de Notas
+        
+    2 - Agenda
+        
+    3 - Tarefas
 
-    if opçao == 1:
+    4 - Sair
+    """)
+    # Escolher uma das opçoes 
+
+    escolha_menu = int(input("Escolha: ").strip())
+
+    # bloco de notas 
+    if escolha_menu == 1:
         print("""
-        Agenda
-    1 - Adicionar compromisso
+    1 - Nova Nota
+
+    2 - Mostrar Notas
             
-    2 - Calendário
+    3 - Editar
             
-    3 - Lembretes
+    4 - Excluir
 
     """)
-        escolha = int(input("Escolha: "))
-        if escolha == 1:
-            data = input("Data: ")
-            hora = input("Hora: ")
-            descriçao = input("Compromisso: ")
-            nova_agenda = data,hora,descriçao
-            agendas.append(nova_agenda)
-            print("Nova agenda adicionda")
-        elif escolha == 2:
-            if len(agendas) == 0:
-                print("Voçê ainda não tem compromisso!")
-            else:
-                 for i, nova_agenda in enumerate(agendas):
-                    data,hora,descriçao = nova_agenda
-                    print(f"{i + 1}. {data} - {hora} - {descriçao}")
 
-    elif opçao == 2:
-        print("""
-    1 - Nova nota 
-            
-    2 - Mostar notas
-            
-    3 - Excluir a ultima nota
+        escolha_notas = int(input("Escolha: ").strip())
 
-    """)
-        escolha1 = int(input("Escolha: "))
-        if escolha1 == 1:
-            nota = input("Comece a escrever: ")
-            notas.append(nota)
-            print("Nova nota adicionda")
-        elif escolha1 == 2:
+        if escolha_notas == 1:
+            nova_nota = input("Digite: ").strip() 
+            notas.append(nova_nota)
+            print("Nota Adicionada com sucesso! ")
+        elif escolha_notas == 2:
             if len(notas) == 0:
-                print("Nenhuma nota salva")
+                print("Não há Notas")
             else:
-                for i, nota in enumerate(notas): 
+                for i, nova_nota in enumerate(notas):
+                    print(f"{i + 1}. {nova_nota}")
+        elif escolha_notas == 3:
+            print("Em Breve")
+        elif escolha_notas == 4:
+            if len(notas) == 0:
+                input("Não há Nota para Excluir! Para continuar pressione <ENTER>")
+            else:
+                for i, nota in enumerate(notas):
                     print(f"{i + 1}. {nota}")
-
-        elif escolha1 == 3:
-            if len(notas) > 0:
-                notas.pop()
-                print("Nota Removida!")
-            else:
-               print("Não a nota para exculir! ")
-        else:
-            print("Escolha invalida!")       
-    elif opçao == 3:
+                excluir_nota = int(input("Qual Nota Voçê Que Excluir? ").strip())
+                print(f"'{notas.pop(excluir_nota - 1)}' Excluido(a) com sucesso!")
+    # agenda 
+    elif escolha_menu == 2:
         print("""
-    1 - Adicionar tarefa
-            
-    2 - Ver tarefas
-            
-    3 - Excluir a ultima tarefa
-
+    1 - Adicionar Agenda
+              
+    2 - Mostrar Agendas
+              
+    3 - Editar
+              
+    4 - Excluir
+              
     """)
-        escolha2 = int(input("Escolha: "))
-        if escolha2 == 1:
-            nova_tarefa = input("se progame: ")  
+        escolha_agenda = int(input("Escolha: ").strip())
+
+        if escolha_agenda == 1:
+            data = input("Data: ")
+            horario = input("Horario: ")
+            descricao = input("Compromisso: ")
+            nova_agenda = (data, horario, descricao)
+            agendas.append(nova_agenda)
+            print("Agenda Adicionada com sucesso!")
+        elif escolha_agenda == 2:
+            if len(agendas) == 0:
+                print("Não há Agendas")
+            else:
+                for i, nova_agenda in enumerate(agendas):
+                    print(f"{i + 1}. {data} | {horario} | {descricao}")
+        elif escolha_agenda == 3:
+            print("Em Breve")
+        elif escolha_agenda == 4:
+            if len(agendas) == 0:
+                input("Não há Nota para Agendas! Para continuar pressione <ENTER>")
+            else:   
+                for i, agenda in enumerate(agendas):
+                     print(f"{i + 1}. {data} | {horario} | {descricao}")
+                excluir_agenda = int(input("Qual Agenda Voçê Que Excluir? ").strip())
+                print(f"{agendas.pop(excluir_agenda - 1)} Excluido(a) com sucesso!")
+    # tarefas
+    elif escolha_menu == 3:
+        print("""
+    1 - Adicionar Tarefa
+              
+    2 - Mostrar Tarefas
+              
+    3 - Editar
+              
+    4 - Excluir
+              
+    """)
+        escolha_tarefa = int(input("Escolha: ").strip())
+
+        if escolha_tarefa == 1:
+            nova_tarefa = input("Digite: ").strip()
             tarefas.append(nova_tarefa)
-            print("Nova Tarefa adiconada!")
-        elif escolha2 == 2:
+            print("Tarefa Adicionada com sucesso!")
+        elif escolha_tarefa == 2:
             if len(tarefas) == 0:
-                print("Você ainda não tem tarefa!")
+                print("Não há Tarefas")
             else:
                 for i, nova_tarefa in enumerate(tarefas):
                     print(f"{i + 1}. {nova_tarefa}")
-        elif escolha2 == 3:
-            if len(tarefas) > 0:
-                tarefas.pop()
-                print("tarefa removida com sucesso!")
+        elif escolha_tarefa == 3:
+            print("Em Breve")
+        elif escolha_tarefa == 4:
+            if len(tarefas) == 0:
+                print("Não há Nota para Agendas! Para continuar pressione <ENTER>")
             else:
-                print("não a tarefa para excluir!")
-        else:
-            print("Escolha invalida!")
-    elif opçao == 4:
-        print("""
-    1 - Em breve ... 
+                for i, tarefa in enumerate(tarefas):
+                    print(f"{i + 1}. {tarefa}")
+                excluir_tarefa = int(input("Qual Agenda Voçê Que Excluir? ").strip())
+                print(f"'{tarefas.pop(excluir_tarefa -1 )}' Excluido(a) com sucesso!")
             
-    """)
-    elif opçao == 5:
-        print("""
-    Fim do progama 
-    """)
+    elif escolha_menu == 4:
+        print("Programa Encerrado ")
         break
+        # conectar ao banco de dados
     else:
-        print("Opção invalida! tente novamente ")
+       input("Opção Invalida, Escolha entre 1 a 4 presione <ENTER>")
